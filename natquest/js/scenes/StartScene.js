dbe253769977c6fec933bbce99c304f43af5cf29
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 03:59:47 -0500

    Update StartScene.js

--- natquest/js/scenes/StartScene.js
+++ natquest/js/scenes/StartScene.js
@@ -4,50 +4,43 @@ class StartScene extends Phaser.Scene {
   }
 
   preload() {
-    // Load the TTF font
-    this.load.bitmapFont('KneWave', 'styles/fonts/Knewave-Regular.ttf';
+    // Load the TTF font using WebFont loader
+    this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
   }
-     this.load.image('background', 'assets/backgrounds/startScreenBackground.png');
 
   create() {
-    // Add background image
-    const background = this.add.image(400, 300, 'background');
-    background.setOrigin(0.5);
+    // Configure the font using the WebFont loader
+    WebFont.load({
+      google: {
+        families: ['KneWave']
+      },
+      active: () => {
+        // After the font is loaded, create your text
+        this.add.text(400, 300, 'Your Game Title', {
+          fontFamily: 'KneWave',
+          fontSize: '48px',
+          fill: '#ffffff',
+          backgroundColor: '#000000',
+          padding: { x: 20, y: 10 },
+        }).setOrigin(0.5);
 
-    // Create text with the custom font
-    const title = this.add.bitmapText(400, 200, 'KneWave', 'NAT QUEST', {
-      fontSize: '48px',
-      fill: '#ffffff',
-      backgroundColor: '#000000',
-      padding: { x: 20, y: 10 },
-    }).setOrigin(0.5);
+        // Add a start button
+        const startButton = this.add.text(400, 400, 'Start', {
+          font: '32px Arial',
+          fill: '#ffffff',
+          backgroundColor: '#000000',
+          padding: { x: 20, y: 10 },
+        })
+          .setOrigin(0.5)
+          .setInteractive();
 
-    // Set up a scaling animation for the title
-    this.tweens.add({
-      targets: title,
-      scaleX: 1.2,
-      scaleY: 1.2,
-      ease: 'Sine.easeInOut',
-      duration: 1000,
-      yoyo: true,
-      repeat: -1,
+        // Set a callback function for the button click event
+        startButton.on('pointerdown', function () {
+          // Transition to the main scene when the button is clicked
+          this.scene.start('WorldScene');
+        }, this);
+      }
     });
-
-    // Add a start button
-    const startButton = this.add.text(400, 400, 'Start', {
-      font: '32px Arial',
-      fill: '#ffffff',
-      backgroundColor: '#000000',
-      padding: { x: 20, y: 10 },
-    })
-      .setOrigin(0.5)
-      .setInteractive();
-
-    // Set a callback function for the button click event
-    startButton.on('pointerdown', function () {
-      // Transition to the main scene when the button is clicked
-      this.scene.start('WorldScene');
-    }, this);
   }
 }
 
