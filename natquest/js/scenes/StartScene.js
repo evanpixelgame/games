f48b16c6202395b8254bf15d45131195b175854e
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 02:01:23 -0500

    Update and rename startScene.js to StartScene.js

---
+++ natquest/js/scenes/StartScene.js
@@ -0,0 +1,24 @@
+class StartScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'StartScene' });
+  }
+
+  create() {
+    // Add a start button
+    const startButton = this.add.text(400, 300, 'Start', {
+      font: '32px Arial',
+      fill: '#ffffff',
+      backgroundColor: '#000000',
+      padding: { x: 20, y: 10 },
+    })
+    .setOrigin(0.5)
+    .setInteractive();
+
+    // Set a callback function for the button click event
+    startButton.on('pointerdown', function () {
+      // Transition to the main scene when the button is clicked
+      this.scene.start('WorldScene');
+    }, this);
+  }
+}
+
