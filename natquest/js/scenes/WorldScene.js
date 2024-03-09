e36b5cdfbe44f6c3de741540a58154b16b455453
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 02:38:53 -0500

    Update WorldScene.js

--- natquest/js/scenes/WorldScene.js
+++ natquest/js/scenes/WorldScene.js
@@ -8,52 +8,53 @@ class WorldScene extends Phaser.Scene {
     this.load.tilemapTiledJSON('map', 'assets/json/map.json');
   }
 
-  create() {
-    // Load tilemap
-    const map = this.make.tilemap({ key: 'map' });
+ create() {
+  const map = this.make.tilemap({ key: "map" });
 
-    // Load tileset
-    const tileset = map.addTilesetImage('tilemap1', 'tiles');
+  // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+  // Phaser's cache (i.e. the name you used in preload)
+  const tileset = map.addTilesetImage("tilemap1", "tiles");
 
-    // Create layers
-    const worldLayer = map.createLayer('Tile Layer 1', tileset, 0, 0);
-    const worldObjectLayer = map.createLayer('Tile Layer 2', tileset, 0, 0);
-    const worldCollisionObjectLayer = map.createLayer('Tile Layer 3', tileset, 0, 0);
+  // Parameters: layer name (or index) from Tiled, tileset, x, y
+  const worldLayer = map.createLayer("Tile Layer 1", tileset, 0, 0);
+  const worldObjectLayer = map.createLayer("Tile Layer 2", tileset, 0, 0);
+  const worldCollisionObjectLayer = map.createLayer("Tile Layer 3", tileset, 0, 0);
 
-    // Phaser supports multiple cameras, but you can access the default camera like this:
-    const camera = this.cameras.main;
 
-    // Create controls
-    const cursors = this.input.keyboard.createCursorKeys();
-    controls = new Phaser.Cameras.Controls.FixedKeyControl({
-      camera: this.cameras.main,
-      left: cursors.left,
-      right: cursors.right,
-      up: cursors.up,
-      down: cursors.down,
-      speed: 0.5,
-    });
+  // Phaser supports multiple cameras, but you can access the default camera like this:
+  const camera = this.cameras.main;
 
-    // Constrain the camera
-    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  // Set up the arrows to control the camera
+  const cursors = this.input.keyboard.createCursorKeys();
+  controls = new Phaser.Cameras.Controls.FixedKeyControl({
+    camera: camera,
+    left: cursors.left,
+    right: cursors.right,
+    up: cursors.up,
+    down: cursors.down,
+    speed: 0.5,
+  });
 
-    // Help text
-    this.add
-      .text(16, 16, 'Arrow keys to scroll', {
-        font: '18px monospace',
-        fill: '#ffffff',
-        padding: { x: 20, y: 10 },
-        backgroundColor: '#000000',
-      })
-      .setScrollFactor(0);
-  }
 
-  update(time, delta) {
-    // Apply the controls to the camera each update tick of the game
-    if (controls) {
-      controls.update(delta);
-    }
-  }
+
+  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+  camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+  // Help text that has a "fixed" position on the screen
+  this.add
+    .text(16, 16, "Arrow keys to scroll", {
+      font: "18px monospace",
+      fill: "#ffffff",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#000000",
+    })
+    .setScrollFactor(0);
+}
+
+
+ update(time, delta) {
+  // Apply the controls to the camera each update tick of the game
+  controls.update(delta);
 }
 
 window.WorldScene = WorldScene;
