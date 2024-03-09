b5330b2605d12595e63adbc59cfd89aa6ec5316a
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 17:39:15 -0500

    Update StartScene.js

--- natquest/js/scenes/StartScene.js
+++ natquest/js/scenes/StartScene.js
@@ -46,7 +46,7 @@ class StartScene extends Phaser.Scene {
     // Set a callback function for the button click event
     startButton.on('pointerdown', function () {
       // Transition to the main scene when the button is clicked
-      this.scene.start('CharacterSelectionScene');
+      this.scene.start('CharSelect');
     }, this);
   }
 }
