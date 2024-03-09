8cfc067e8240c39d5d5aef30296d328d41604472
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 03:48:28 -0500

    Update StartScene.js

--- natquest/js/scenes/StartScene.js
+++ natquest/js/scenes/StartScene.js
@@ -4,14 +4,8 @@ class StartScene extends Phaser.Scene {
   }
 
   preload() {
-    // Load background image
-    this.load.image('background', 'assets/backgrounds/startScreenBackground.png');
     // Load the TTF font
-    this.load.rexWebFont({
-      key: 'KneWave',
-      urls: ['styles/fonts/Knewave-Regular.ttf'],
-      // Specify other font properties if needed
-    });
+    this.load.bitmapFont('KneWave', 'path/to/KneWave/font.png', 'path/to/KneWave/font.xml');
   }
 
   create() {
@@ -19,31 +13,28 @@ class StartScene extends Phaser.Scene {
     const background = this.add.image(400, 300, 'background');
     background.setOrigin(0.5);
 
-    // Add a title
-    const title = this.add.text(400, 200, 'NAT QUEST', {
-      fontFamily: 'KneWave',
+    // Create text with the custom font
+    const title = this.add.bitmapText(400, 200, 'KneWave', 'Your Game Title', {
       fontSize: '48px',
       fill: '#ffffff',
       backgroundColor: '#000000',
       padding: { x: 20, y: 10 },
-    })
-      .setOrigin(0.5);
+    }).setOrigin(0.5);
 
     // Set up a scaling animation for the title
     this.tweens.add({
       targets: title,
-      scaleX: 1.2,  // Scale up by 20%
+      scaleX: 1.2,
       scaleY: 1.2,
       ease: 'Sine.easeInOut',
       duration: 1000,
-      yoyo: true,  // Scale back to the original size
-      repeat: -1,  // Infinite loop
+      yoyo: true,
+      repeat: -1,
     });
 
     // Add a start button
-    const startButton = this.add.text(400, 200, 'Start', {
-      font: '32px',
-      fontFamily: 'KneWave',
+    const startButton = this.add.text(400, 400, 'Start', {
+      font: '32px Arial',
       fill: '#ffffff',
       backgroundColor: '#000000',
       padding: { x: 20, y: 10 },
