ddfafcfa1b1423a4a503ec1051cc5d39bd0be658
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 05:51:52 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -1,3 +1,10 @@
+const globalData = new DataPlugin(); // Create a global data manager (can create additional local instances
+//too by making more new DataPlugin classes in each scene that needs its own data management)
+// Access the data manager using `this.dataManager`
+this.globalData.set('health', 100); // Store health
+const playerHealth = this.dataManager.get('health'); // Retrieve health
+// ... and so on for other data keys ...
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
