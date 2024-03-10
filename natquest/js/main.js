f07fc3506c01744365881f18b2d7d672fdd1df64
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 08:57:21 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -8,8 +8,8 @@ const config = {
   scale: {
     //mode: Phaser.Scale.RESIZE,
     //autoCenter: Phaser.Scale.CENTER_BOTH
-    mode: Phaser.Scale.LANDSCAPE,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    //mode: Phaser.Scale.LANDSCAPE,
+    //autoCenter: Phaser.Scale.CENTER_BOTH
   },
   scene: [
     Preloader,
