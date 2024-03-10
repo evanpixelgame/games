2a1eb330107affbd069d3101896ed2adbd2d1da0
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 06:51:51 -0400

    Create gameState.js

---
+++ natquest/js/gameState.js
@@ -0,0 +1,10 @@
+class GameManager {
+  constructor() {
+    this.selectedCharacter = null;
+    this.playerName = '';
+    this.health = 100;
+    this.stamina = 100;
+  }
+}
+
+const gameManager = new GameManager();
