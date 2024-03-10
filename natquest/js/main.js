e89b5b6a4cf1ba378db339bc536d1addd33700d7
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 06:52:10 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -1,14 +1,3 @@
-class GameManager {
-  constructor() {
-    this.selectedCharacter = null;
-    this.playerName = '';
-    this.health = 100;
-    this.stamina = 100;
-  }
-}
-
-const gameManager = new GameManager();
-
 const config = {
   type: Phaser.AUTO,
   width: 800,
