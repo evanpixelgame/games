fa1e07660f3d388843f64e2640161958f5312563
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 09:12:00 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -1,13 +1,12 @@
 class OpenWorld extends Phaser.Scene {
   constructor() {
     super({ key: 'OpenWorld' });
-    
     // Declare controls as a property of the class
     this.controls = null;
   }
 
   preload() {
-  
+    // Load assets if needed
   }
 
   create() {
@@ -22,51 +21,7 @@ class OpenWorld extends Phaser.Scene {
     const worldObjectLayer = map.createLayer('Tile Layer 2', tileset, 0, 0);
     const worldCollisionObjectLayer = map.createLayer('Tile Layer 3', tileset, 0, 0);
 
-    // Phaser supports multiple cameras, but you can access the default camera like this:
-    const camera = this.cameras.main;
-
-    this.input.addPointer(3); // Add up to 3 pointers for multi-touch support
-
-  // Create controls for both keyboard and mobile
-  const cursors = this.input.keyboard.createCursorKeys();
-  this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
-    camera: this.cameras.main,
-    left: cursors.left,
-    right: cursors.right,
-    up: cursors.up,
-    down: cursors.down,
-    speed: 0.5,
-  });
-
-  // Handle mobile input
-  this.input.on('pointermove', (pointer) => {
-    if (pointer.isDown) {
-      // Use the distance moved by the pointer to control camera movement
-      const deltaX = pointer.x - pointer.prevPosition.x;
-      const deltaY = pointer.y - pointer.prevPosition.y;
-
-      // Adjust the camera position based on pointer movement
-      this.cameras.main.scrollX += deltaX;
-      this.cameras.main.scrollY += deltaY;
-    }
-  });
-
-  // ... (existing code)
-}
-
-update(time, delta) {
-  // Apply the controls to the camera each update tick of the game
-  if (this.controls) {
-    this.controls.update(delta);
-
-    // Adjust the camera position for mobile controls
-    if (this.input.activePointer.isDown) {
-      const deltaX = this.input.activePointer.x - this.input.activePointer.prevPosition.x;
-      const deltaY = this.input.activePointer.y - this.input.activePointer.prevPosition.y;
-
-      this.cameras.main.scrollX += deltaX;
-      this.cameras.main.scrollY += deltaY;
-    }
-  }
-
-window.OpenWorld = OpenWorld;
+    // Create controls for both keyboard and mobile
+    const cursors = this.input.keyboard.createCursorKeys();
+    this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
+      camera:
