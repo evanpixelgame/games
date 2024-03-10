7d1b794e3da66a6cf29c15c200dea2426f0d5319
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-09 23:59:53 -0500

    Update CharSelect.js

--- natquest/js/scenes/gameStartScenes/CharSelect.js
+++ natquest/js/scenes/gameStartScenes/CharSelect.js
@@ -14,7 +14,7 @@ class CharSelect extends Phaser.Scene {
 
   create() {
 
-    const background = this.add.image(400, 300, 'background').setOrigin(0.5);
+  
     
     // Display character options
     const character1 = this.add.image(200, 200, 'character1').setInteractive();
