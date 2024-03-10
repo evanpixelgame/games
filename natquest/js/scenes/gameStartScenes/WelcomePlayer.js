4a1a0c16a734475ab478a647d5efbf8a98f60f0e
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-10 06:28:14 -0400

    Update WelcomePlayer.js

--- natquest/js/scenes/gameStartScenes/WelcomePlayer.js
+++ natquest/js/scenes/gameStartScenes/WelcomePlayer.js
@@ -1,1 +1,13 @@
 
+preload() {}
+
+create() {
+const heartButton = this.add.text(385, 550, gameManager.playerName, {
+      fontSize: '48px', 
+      fontFamily: 'knewave',
+      fill: '#c92b23',
+      padding: { x: 20, y: 20 },
+    })
+      .setOrigin(0.5)
+      .setInteractive();
+}
