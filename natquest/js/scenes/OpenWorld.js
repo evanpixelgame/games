2e5de25b16631a4ca58006e537e84a3b9e8c05b1
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-11 18:17:56 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -26,41 +26,44 @@ class OpenWorld extends Phaser.Scene {
     };
   }
 
-  createVirtualJoystick() {
+    createVirtualJoystick() {
     this.joyStick = this.plugins.get('rex-virtual-joystick-plugin"').add(
-        this,
-        Object.assign({}, this.joystickConfig, {
-            radius: 32,
-            base: this.add.image(0, 0, 'base').setDisplaySize(110, 110),
-            thumb: this.add.image(0, 0, 'thumb').setDisplaySize(48, 48)
-        })
+      this,
+      Object.assign({}, this.joystickConfig, {
+        radius: 32,
+        base: this.add.image(0, 0, 'base').setDisplaySize(110, 110),
+        thumb: this.add.image(0, 0, 'thumb').setDisplaySize(48, 48)
+      })
     ).on('update', this.updateJoystickState, this);
+
     this.cursorKeys = this.joyStick.createCursorKeys();
 
     // Listener event to reposition virtual joystick
     // whatever place you click in the game area
     this.input.on('pointerdown', pointer => {
-        this.joyStick.x = pointer.x;
-        this.joyStick.y = pointer.y;
-        this.joyStick.base.x = pointer.x;
-        this.joyStick.base.y = pointer.y;
-        this.joyStick.thumb.x = pointer.x;
-        this.joyStick.thumb.y = pointer.y;
+      this.joyStick.x = pointer.x;
+      this.joyStick.y = pointer.y;
+      this.joyStick.base.x = pointer.x;
+      this.joyStick.base.y = pointer.y;
+      this.joyStick.thumb.x = pointer.x;
+      this.joyStick.thumb.y = pointer.y;
     });
 
-    // Listener event to return virtual 
+    // Listener event to return virtual
     // joystick to its original position
     this.input.on('pointerup', pointer => {
-        this.joyStick.x = this.staticXJsPos;
-        this.joyStick.y = this.staticYJsPos;
-        this.joyStick.base.x = this.staticXJsPos;
-        this.joyStick.base.y = this.staticYJsPos;
-        this.joyStick.thumb.x = this.staticXJsPos;
-        this.joyStick.thumb.y = this.staticYJsPos;
-        this.lastCursorDirection = "center";
-        this.setCursorDebugInfo();
+      this.joyStick.x = this.staticXJsPos;
+      this.joyStick.y = this.staticYJsPos;
+      this.joyStick.base.x = this.staticXJsPos;
+      this.joyStick.base.y = this.staticYJsPos;
+      this.joyStick.thumb.x = this.staticXJsPos;
+      this.joyStick.thumb.y = this.staticYJsPos;
+      this.lastCursorDirection = "center";
+      this.setCursorDebugInfo();
     });
-      setCursorDebugInfo() {
+  }
+
+  setCursorDebugInfo() {
     const force = Math.floor(this.joyStick.force * 100) / 100;
     const angle = Math.floor(this.joyStick.angle * 100) / 100;
     let text = `Direction: ${this.lastCursorDirection}\n`;
@@ -69,6 +72,7 @@ class OpenWorld extends Phaser.Scene {
     text += `FPS: ${this.sys.game.loop.actualFps}\n`;
     this.cursorDebugText.setText(text);
   }
+}
 
 
   preload() {
