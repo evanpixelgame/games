67e59e9ceb51a80f7d0b22915c154122eb7b42d1
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-11 06:26:35 -0400

    Update CharSelect.js

--- natquest/js/scenes/gameStartScenes/CharSelect.js
+++ natquest/js/scenes/gameStartScenes/CharSelect.js
@@ -32,12 +32,12 @@ character1.setScale(2.8); // Adjust the scale factor (0.5 scales to half the siz
 character2.setScale(2.8); 
 character3.setScale(2.8);
 
+    
       // Add some text to the backdrop
   const instructionText = this.add.text(400, 270, 'Pick a character', {
     fontSize: '32px',
-    fontWeight: 'bold',
-    fontFamily: 'Arial',
-    fill: 'purple',
+    fontFamily: 'knewave',
+    fill: '#c92b23',
     align: 'center',
   })
     .setOrigin(0.5);
