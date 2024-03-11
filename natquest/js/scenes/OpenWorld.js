55046aab80ef3603f69f5536d23cb9350e187665
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-11 09:58:00 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -51,7 +51,7 @@ const testSprite = this.add.sprite(200, 200, 'babyMouse');
 console.log(testSprite); // Log the sprite object to the console
 
     // Enable physics for the sprite if needed
-    this.physics.world.enable(this.testSprite);
+    this.physics.world.enable(testSprite);
 
     // Set camera to follow the sprite
     this.cameras.main.startFollow(this.testSprite);
