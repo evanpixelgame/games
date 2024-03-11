8b2ecd43f4c223e659d6648d2076e33c5e132146
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-11 07:25:21 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -11,6 +11,12 @@ const config = {
    mode: Phaser.Scale.FIT,
    // autoCenter: Phaser.Scale.CENTER_BOTH
   },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
   scene: [
     Preloader,
     StartMenu,
