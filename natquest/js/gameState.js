9a9703eaa924f3456b9dba0c67bc4349fcaeb2aa
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 07:33:43 -0400

    Update gameState.js

--- natquest/js/gameState.js
+++ natquest/js/gameState.js
@@ -1,6 +1,7 @@
 class GameManager {
   constructor() {
     this.selectedCharacter = null;
+    this.selectedCharacterIndex = null;
     this.playerName = '';
     this.health = 100;
     this.stamina = 100;
