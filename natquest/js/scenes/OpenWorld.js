35022624b1ebe3abf849d2171e6d67a4e457b212
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 06:21:56 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -5,7 +5,7 @@ class OpenWorld extends Phaser.Scene {
     this.controls = null;
     this.isMobile = null;
     this.map = null;
-    this.playerChar = 'Confused Woman';
+    this.playerChar = this.selectedChar;
   }
 
   
