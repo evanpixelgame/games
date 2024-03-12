1761717753006b52562d0592b4d0be5ee8275657
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 08:09:21 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -25,7 +25,9 @@ class OpenWorld extends Phaser.Scene {
           frameWidth: 64,
           frameHeight: 64
       }); 
-    } else if (gameManager.selectedCharacter == 'Confused Woman') {
+    } 
+    if (gameManager.selectedCharacter == 'Confused Woman') {
+      console.log('gameManager.selectedCharacter);
     
        this.load.spritesheet("player", "assets/sprites/player/confusedWoman.png", {
           frameWidth: 64,
