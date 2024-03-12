0994720b5442159dd21d6e237220af53e3460691
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 07:26:45 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -16,7 +16,10 @@ class OpenWorld extends Phaser.Scene {
      // endFrame: 272     // The last frame to end with (optional)
      // this.load.image('base', 'assets/images/base.png');
      // this.load.image('thumb', 'assets/images/thumb.png');
-     // this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
+    // this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
+    
+    console.log(this.selectedCharacter);
+    
     if (this.selectedCharacter == 'Baby Mouse') {  
     this.load.spritesheet("player", "assets/sprites/player/babyMouse.png", {
           frameWidth: 64,
