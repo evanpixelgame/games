4d6398676044273252ab33dd7358bfaff74adb91
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 06:39:40 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -26,18 +26,23 @@ class OpenWorld extends Phaser.Scene {
       this.load.image('base', 'assets/images/base.png');
       this.load.image('thumb', 'assets/images/thumb.png');
       this.load.plugin('rexvirtualjoystickplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexvirtualjoystickplugin.min.js', true);
-      this.load.spritesheet("player", "assets/sprites/player/babyMouse.png", {
+    if (this.selectedCharacter == 'Baby Mouse' {  
+    this.load.spritesheet("player", "assets/sprites/player/babyMouse.png", {
+          frameWidth: 64,
+          frameHeight: 64
+      }); 
+    } else if (this.selectedCharacter == 'Confused Woman') {
+    
+       this.load.spritesheet("player", "assets/sprites/player/confusedWoman.png", {
           frameWidth: 64,
           frameHeight: 64
       });
-       this.load.spritesheet("player2", "assets/sprites/player/confusedWoman.png", {
+    } else {
+       this.load.spritesheet("player", "assets/sprites/player/fatWolf.png", {
           frameWidth: 64,
           frameHeight: 64
       });
-       this.load.spritesheet("player3", "assets/sprites/player/fatWolf.png", {
-          frameWidth: 64,
-          frameHeight: 64
-      });
+    }
   }
 
 
