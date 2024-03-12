00982708d1f31cf25d7c587a6cd7864aa9e9ebc2
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 05:02:04 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -83,9 +83,11 @@ class OpenWorld extends Phaser.Scene {
   }
 
   createAnimations() {
+const spriteSheetName = this.selectedCharacter;
+    
     this.anims.create({
         key: 'walking-up',
-        frames: this.anims.generateFrameNames(this.selectedCharacter, {
+        frames: this.anims.generateFrameNames(spriteSheetName, {
             frames: [
               104, 105, 106, 107, 108, 109, 110, 111, 112
             ]
@@ -97,7 +99,7 @@ class OpenWorld extends Phaser.Scene {
 
     this.anims.create({
         key: 'walking-left',
-        frames: this.anims.generateFrameNames(this.selectedCharacter, {
+        frames: this.anims.generateFrameNames(spriteSheetName, {
             frames: [
               117, 118, 119, 120, 121, 122, 123, 124, 125
             ]
@@ -109,7 +111,7 @@ class OpenWorld extends Phaser.Scene {
 
     this.anims.create({
         key: 'walking-down',
-        frames: this.anims.generateFrameNames(this.selectedCharacter, {
+        frames: this.anims.generateFrameNames(spriteSheetName, {
             frames: [
               130, 131, 132, 133, 134, 135, 136, 137, 138
             ]
@@ -121,7 +123,7 @@ class OpenWorld extends Phaser.Scene {
 
     this.anims.create({
         key: 'walking-right',
-        frames: this.anims.generateFrameNames(this.selectedCharacter, {
+        frames: this.anims.generateFrameNames(spriteSheetName, {
             frames: [
               143, 144, 145, 146, 147, 148, 149, 150, 151 
             ]
