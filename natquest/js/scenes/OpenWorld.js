e36522fea6ac80e0a6638d75543a2b00cb860698
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-13 18:57:38 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -114,13 +114,16 @@ class OpenWorld extends Phaser.Scene {
    if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
     // Code for Android or iOS
    this.scale.scaleMode = Phaser.Scale.ScaleModes.RESIZE;
+        const centerX = gameSize.width / 2;
+        const centerY = gameSize.height / 2;
+        this.player.setPosition(centerX, centerY);
        // Check the current orientation
-        const currentOrientation = this.scale.orientation;
+   //     const currentOrientation = this.scale.orientation;
 
         // If the current orientation is portrait, switch to landscape
-        if (currentOrientation === Phaser.Scale.Orientation.PORTRAIT) {
-            this.scale.setOrientation(Phaser.Scale.Orientation.LANDSCAPE);
-        }
+     //   if (currentOrientation === Phaser.Scale.Orientation.PORTRAIT) {
+       //     this.scale.setOrientation(Phaser.Scale.Orientation.LANDSCAPE);
+        //}
     this.scene.launch('MobileControls');
 
    // this.scale.scaleMode = Phaser.Scale.ScaleModes.RESIZE;
