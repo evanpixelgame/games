bfad16571e38fe5b163f2cd5d62610e2521ab8fc
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 22:44:49 -0400

    Update MobileControls.js

--- natquest/js/scenes/MobileControls.js
+++ natquest/js/scenes/MobileControls.js
@@ -17,7 +17,7 @@ class MobileControls extends Phaser.Scene {
     create() {
         console.log('pleasewritethis');
             const posX = this.game.config.width / 5;
-    const posY = this.game.config.width - this.game.config.height / 3;
+    const posY = this.game.config.height / 3;
         this.joyStick = this.plugins.get('rexvirtualjoystickplugin').add(this, {
                 x: posX,
                 y: posY,
