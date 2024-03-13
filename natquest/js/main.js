fc6984aa4f8dda8d0999e936624c5a816447cfdf
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-13 16:58:26 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -8,6 +8,8 @@ const config = {
   parent: 'game-container',
   pixelArt: true,
   scale: {
+     scale: {
+        orientation: Phaser.Scale.Orientation.LANDSCAPE, // Adjust to your preference
     //mode: Phaser.Scale.RESIZE,
     //autoCenter: Phaser.Scale.CENTER_BOTH
    mode: Phaser.Scale.FIT,
