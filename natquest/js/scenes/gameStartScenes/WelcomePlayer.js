9c83a3a48f0b419820194a711e6f8e6edddfb8be
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 21:08:54 -0400

    Update WelcomePlayer.js

--- natquest/js/scenes/gameStartScenes/WelcomePlayer.js
+++ natquest/js/scenes/gameStartScenes/WelcomePlayer.js
@@ -17,7 +17,7 @@ class WelcomePlayer extends Phaser.Scene {
 
     beginButton.on('pointerdown', function () {
       // Transition to the main scene when the button is clicked
-      this.scene.start('MobileControls');
+      this.scene.start('OpenWorld');
     }, this);
 
     // Declaration and initialization of welcomeTextBlock
