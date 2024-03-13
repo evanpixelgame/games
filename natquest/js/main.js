e3906f5e3c6f0a5f46d29a85f49f7971c597d256
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-12 22:53:04 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -1,5 +1,7 @@
-const width = 800; //800
-const height = 600; //600
+//const width = 800; //800
+//const height = 600; //600
+const width = window.innerWidth;
+const height = window.innerHeight;
 const config = {
   type: Phaser.AUTO,
   width: width,
