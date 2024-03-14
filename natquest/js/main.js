10c603aaf7733438a57df7181822539e735878c3
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 08:37:54 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -4,8 +4,8 @@ const height = window.innerHeight;
 //const height = 600;
 const config = {
   type: Phaser.AUTO,
-  width: 200,
-  height: 200,
+  width: 800,
+  height: 600,
   parent: 'game-container',
   pixelArt: true,
   scale: {
