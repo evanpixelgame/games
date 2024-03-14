02fd507bec05a0c29b9cb522469f95fd315628a8
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-13 22:24:37 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -9,7 +9,7 @@ const config = {
   parent: 'game-container',
   pixelArt: true,
   scale: {
-    //mode: Phaser.Scale.RESIZE,
+    mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     mode: Phaser.Scale.FIT,
    // autoCenter: Phaser.Scale.CENTER_BOTH
