0f273281d99a1ea4d24678b247507e73d38798ff
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 02:23:42 -0400

    Update WelcomePlayer.js

--- natquest/js/scenes/gameStartScenes/WelcomePlayer.js
+++ natquest/js/scenes/gameStartScenes/WelcomePlayer.js
@@ -29,6 +29,7 @@ class WelcomePlayer extends Phaser.Scene {
   }
 
   create() {
+    this.openWorldScene = this.scene.get('OpenWorld'); //DELETE IF THIS DOESNT WORK
     const beginButton = this.add.text(385, 550, 'Click Here To Begin Game!', {
       fontSize: '48px', 
       fontFamily: 'knewave',
