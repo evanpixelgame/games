c2e81024dd861ca0cf6ee5ec3b1bd09363d252a1
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 09:03:34 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -54,9 +54,8 @@ class OpenWorld extends Phaser.Scene {
 
   update(time, delta) {
     this.cameras.main.centerOn(this.player.x, this.player.y);
-    this.cameras.main.setSnap(window.innerWidth / 2, window.innerHeight / 2);
        this.game.config.width = window.innerHeight;
-        game.config.height
+        game.config.height = window.innerWidth
    // this.cameras.main.scrollY = this.player.y + 200;
   }
   
