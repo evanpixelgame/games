0badb04d62a9148704a202036b8a7840937850bc
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 00:47:46 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -37,29 +37,7 @@ class OpenWorld extends Phaser.Scene {
   create() {
     
     this.scene.launch('MobileControls');
-    
-    // COMPUTER/TV SCREEN SPECIFIC LOGIC 
-  if (!this.sys.game.device.os.android && !this.sys.game.device.os.iOS) {
-     this.scale.setGameSize(window.innerWidth, window.innerHeight); 
-      // Help text
-  this.add
-    .text(16, 16, 'WASD to move', {
-      font: '18px monospace',
-      fill: '#ffffff',
-      padding: { x: 20, y: 10 },
-      backgroundColor: '#000000',
-    })
-    .setScrollFactor(0); 
-  }
-
-    
-    //    this.scale.scaleMode = Phaser.Scale.ScaleModes.RESIZE;
-    
-        // Center game objects (optional)
-      //  this.scale.pageAlignHorizontally = true;
-      //  this.scale.pageAlignVertically = true;
-
-
+    this.scene.launch('ComputerControls');
     
   const map = this.make.tilemap({ key: 'map' });
   // Load tileset
@@ -88,14 +66,6 @@ class OpenWorld extends Phaser.Scene {
     this.player.setCollideWorldBounds(true);
 
      this.speed = 200;
-
-  // Create controls for arrow keys and WASD
-  this.cursors = this.input.keyboard.addKeys({
-    up: Phaser.Input.Keyboard.KeyCodes.W,
-    down: Phaser.Input.Keyboard.KeyCodes.S,
-    left: Phaser.Input.Keyboard.KeyCodes.A,
-    right: Phaser.Input.Keyboard.KeyCodes.D,
-  });
 
   // Constrain the camera
   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
@@ -155,28 +125,6 @@ class OpenWorld extends Phaser.Scene {
   } // <=== create() end tag
 
   update(time, delta) {
-
-    if (this.cursors.up.isDown) {
-      this.player.setVelocityY(-this.speed);
-       this.player.anims.play('walking-down', true);
-    } else if (this.cursors.down.isDown) {
-      this.player.setVelocityY(this.speed);
-       this.player.anims.play('walking-up', true);
-    } else {
-      this.player.setVelocityY(0);
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-this.speed);
-      this.player.anims.play('walking-left', true);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(this.speed);
-      this.player.anims.play('walking-right', true);
-    } else {
-      this.player.setVelocityX(0);
-    }
-    
-    this.cameras.main.startFollow(this.player);
     
   }
 }
