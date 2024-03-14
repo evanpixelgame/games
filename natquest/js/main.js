02aa518b404b7b2f952d486d7aa963a2d9fc72aa
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 17:49:02 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -12,7 +12,7 @@ const config = {
   //  mode: Phaser.Scale.LANDSCAPE,
 //  mode: Phaser.Scale.RESIZE,
    mode: Phaser.Scale.ScaleModes.WIDTH_CONTROLS_HEIGHT,
-     autoCenter: Phaser.Scale.CENTER_BOTH,
+  autoCenter: Phaser.Scale.CENTER_BOTH,
   // autoCenter: Phaser.Scale.CENTER_VERTICALLY,
   //  autoCenter: Phaser.Scale.CENTER_BOTH,
    // mode: Phaser.Scale.FIT,
