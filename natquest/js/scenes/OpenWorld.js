c06cf08c294bf2b991df474c9163c94f8df86151
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 19:05:32 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -15,6 +15,8 @@ class OpenWorld extends Phaser.Scene {
 
   create() {
 
+// this.scene.launch('MobileControls', { player: this.player, speed: this.speed }); //FOR TESTING
+    
      if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
        }
