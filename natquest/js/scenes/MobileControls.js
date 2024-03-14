dc6bb07a9a06487f1d8f8729dcfaab6b43ed227b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-14 03:40:11 -0400

    Update MobileControls.js

--- natquest/js/scenes/MobileControls.js
+++ natquest/js/scenes/MobileControls.js
@@ -18,10 +18,15 @@ class MobileControls extends Phaser.Scene {
     }
 
     create() {
-    this.computerControlsScene = this.scene.get('ComputerControls');
-    this.openWorldScene = this.scene.get('OpenWorld'); //DELETE IF THIS DOESNT WORK
-    this.player = this.scene.get('OpenWorld').player;  
-    this.speed = this.openWorldScene.speed;
+    //this.computerControlsScene = this.scene.get('ComputerControls');
+  //  this.openWorldScene = this.scene.get('OpenWorld'); //DELETE IF THIS DOESNT WORK
+ //   this.player = this.scene.get('OpenWorld').player;  
+ //   this.speed = this.openWorldScene.speed;
+
+       this.openWorldScene = this.scene.get('OpenWorld');
+        this.player = this.openWorldScene.player;
+        this.speed = this.openWorldScene.speed;
+        
     const posX = 80; //this.game.config.width / 5;
     const posY = 80;
 
