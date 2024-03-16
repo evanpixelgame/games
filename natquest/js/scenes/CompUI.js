92f000b26000d653dab1ed3dbc4538834e571182
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-16 01:30:16 -0400

    Create CompUI.js

---
+++ natquest/js/scenes/CompUI.js
@@ -0,0 +1,38 @@
+
+class CompUI extends Phaser.Scene {
+  constructor() {
+    super({ key: 'CompUI' });
+  }
+      
+  preload() {
+
+  }
+
+  create() {
+
+        this.openWorldScene = this.scene.get('OpenWorld');
+        this.player = this.openWorldScene.player;
+    //    this.speed = this.openWorldScene.speed;
+
+     const startMenuScene = this.scene.get('StartMenu');
+    
+     const fullscreenButton = this.add.text(120, 30, 'Fullscreen', {
+      fontSize: '22px', 
+      fontFamily: 'Roboto',
+      fill: '#ffffff',
+      padding: { x: 20, y: 20 },
+    })
+      .setOrigin(0.5)
+      .setInteractive();
+
+ //   this.scale.on('fullscreenchange', this.handleFullscreenChange.bind(this));
+    this.scale.on('resize', startMenuScene.handleFullscreenChange, this);
+
+  }
+
+  update(time, delta) {
+
+  }
+
+}
+window.CompUI = CompUI;
