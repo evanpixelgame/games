1531c7a5d0f0b179638eb70b789e9beae7b29b7f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-16 15:25:53 -0400

    Update CompUI.js

--- natquest/js/scenes/CompUI.js
+++ natquest/js/scenes/CompUI.js
@@ -21,17 +21,20 @@ class CompUI extends Phaser.Scene {
     const vh = window.innerHeight;
 
     // ****************************************************************TOP BAR UI ICONS*************************************************************
-
-    const settingsIcon = this.add.sprite(6.5 * vw / 9, 50, 'settingsIcon').setInteractive();
+      
+    
+        const fullscreenIcon = this.add.sprite(5 * vw/ 9, 50, 'infoIcon').setInteractive();
+        const settingsIcon = this.add.sprite(6.5 * vw / 9, 50, 'settingsIcon').setInteractive();
         const zoomInIcon = this.add.sprite(7 * vw / 9, 50, 'zoomInIcon').setInteractive();
         const zoomOutIcon = this.add.sprite(6 * vw / 9, 50, 'zoomOutIcon').setInteractive();   //was at 7.5 vw changed temp for diagnosis
         const fullscreenIcon = this.add.sprite(8.1 * vw/ 9, 50, 'fullscreenIcon').setInteractive();
 
+       infoIcon.setScale(.12);
        settingsIcon.setScale(0.1);
         zoomInIcon.setScale(0.2);
         zoomOutIcon.setScale(0.2);
         fullscreenIcon.setScale(.12);
-
+    
 settingsIcon.on('pointerdown', () => {
     // Get the position of the settings icon
     const { x, y } = settingsIcon;
@@ -115,6 +118,12 @@ settingsIcon.on('pointerdown', () => {
                 } else {
               this.requestFullScreen();
                 }
+                    });
+
+
+            infoIcon.on('pointerdown', () => {
+            // Handle fullscreen icon click
+            console.log('info icon clicked');
                     });
 
             
