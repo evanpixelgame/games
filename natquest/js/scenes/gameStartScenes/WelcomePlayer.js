c2889ec5f04a96140408519c79f57159d91d5a52
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-15 20:41:05 -0400

    Update WelcomePlayer.js

--- natquest/js/scenes/gameStartScenes/WelcomePlayer.js
+++ natquest/js/scenes/gameStartScenes/WelcomePlayer.js
@@ -34,11 +34,17 @@ class WelcomePlayer extends Phaser.Scene {
         const vw = window.innerWidth;
         const xMid = vw * .5;
         const vh = window.innerHeight;
+
+     const graphics = this.add.graphics();
+
+    // Fill the background with a solid color
+    graphics.fillStyle(0xfdd5d5); // Specify the color (black in this case)
+    graphics.fillRect(0, 0, this.game.config.width, this.game.config.height);
     
     //this.openWorldScene = this.scene.get('OpenWorld'); //DELETE IF THIS DOESNT WORK
   //  this.openWorldScene = this.scene.get('ComputerControls'); 
  //   const beginButton = this.add.text(385, 550, 'Click Here To Begin Game!', {
-     const beginButton = this.add.text(50, 100, 'Start', {
+     const beginButton = this.add.text(xMid, vh * .8, 'Start', {
       fontSize: '48px', 
       fontFamily: 'knewave',
       fill: '#c92b23',
