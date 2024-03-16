cda6b3fecd3210c2ca9fc85d292695173588538d
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-16 02:04:05 -0400

    Update CompUI.js

--- natquest/js/scenes/CompUI.js
+++ natquest/js/scenes/CompUI.js
@@ -16,7 +16,11 @@ class CompUI extends Phaser.Scene {
 
      const startMenuScene = this.scene.get('StartMenu');
 
-        const fullscreenButton = this.add.text(120, 50, 'Fullscreen', {
+        const vw = window.innerWidth;
+    const xMid = vw * .5;
+    const vh = window.innerHeight;
+
+      const fullscreenButton = this.add.text(xMid, 50, 'Fullscreen', {
       fontSize: '26px', 
       fontFamily: 'knewave',
       fill: '#c92b23',
@@ -36,10 +40,71 @@ class CompUI extends Phaser.Scene {
 
     
  //   this.scale.on('fullscreenchange', this.handleFullscreenChange.bind(this));
-    this.scale.on('resize', startMenuScene.handleFullscreenChange, this);
+    this.scale.on('resize', this.handleFullscreenChange, this);
 
   }
 
+ requestFullScreen() {
+    const element = document.documentElement;
+
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { /* Firefox */
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { /* IE/Edge */
+        element.msRequestFullscreen();
+    }
+}
+
+   handleFullscreenChange() {
+     console.log('before if logic');
+        if (this.scale.isFullscreen) {
+            console.log('Entering fullscreen mode');
+            this.resizeGame({ width: window.innerWidth, height: window.innerHeight });
+        } else {
+            console.log('Exiting fullscreen mode');
+            this.resizeGame({ width: window.innerWidth, height: window.innerHeight });
+        }
+    }
+
+      resizeGame(gameSize) {
+        console.log('attempting resize with resizeGame method');
+        const { width, height } = gameSize;
+
+        // Resize the game canvas
+        this.sys.game.canvas.style.width = width + 'px';
+        this.sys.game.canvas.style.height = height + 'px';
+
+        // Resize the game config to match the new size
+        this.sys.game.config.width = width;
+        this.sys.game.config.height = height;
+
+        // Call resize events on all scenes
+        this.events.emit('resize', gameSize);
+    }
+
+  isFullScreen() {
+    return (
+        document.fullscreenElement ||
+        document.mozFullScreenElement ||
+        document.webkitFullscreenElement ||
+        document.msFullscreenElement
+    );
+}
+
+exitFullScreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
 
   
 
