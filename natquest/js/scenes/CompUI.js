d64ec8c5736ade637f1cbfbf27bd4dfee3eeb34b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-16 01:57:04 -0400

    Update CompUI.js

--- natquest/js/scenes/CompUI.js
+++ natquest/js/scenes/CompUI.js
@@ -15,20 +15,33 @@ class CompUI extends Phaser.Scene {
         this.speed = this.openWorldScene.speed;
 
      const startMenuScene = this.scene.get('StartMenu');
-    
-     const fullscreenButton = this.add.text(100, 100, 'Fullscreen', {
-      fontSize: '22px', 
-      fontFamily: 'Roboto',
-      fill: '#ffffff',
+
+        const fullscreenButton = this.add.text(120, 50, 'Fullscreen', {
+      fontSize: '26px', 
+      fontFamily: 'knewave',
+      fill: '#c92b23',
       padding: { x: 20, y: 20 },
     })
       .setOrigin(0.5)
       .setInteractive();
 
+
+     fullscreenButton.on('pointerdown', () => {
+    if (this.isFullScreen()) {
+        this.exitFullScreen();
+    } else {
+        this.requestFullScreen();
+    }
+});
+
+    
  //   this.scale.on('fullscreenchange', this.handleFullscreenChange.bind(this));
     this.scale.on('resize', startMenuScene.handleFullscreenChange, this);
 
   }
+
+
+  
 
   update(time, delta) {
 
