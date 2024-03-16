0cddb2ef02bd1047a885def055ca04d50524b92a
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-15 20:05:02 -0400

    Create Settings.js

---
+++ natquest/js/scenes/Settings.js
@@ -0,0 +1,13 @@
+add settings options here 
+
+background music volume
+Noise/sound effects volume
+Accessibility Controls
+    -screen reader
+    -additional magnification
+    
+-UI interface size
+-Text font size
+-maybe get into fun specifics like color themes, font choices, other stylistic things
+
+-Location where to save local files/browser files if browser saves need a location which they may not
