a1a48afed8ec9d95620b3ea54a321b01b0787543
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-20 03:03:36 -0400

    Create collisionHandler.js

---
+++ natquest/js/scenes/collisionHandler.js
@@ -0,0 +1,36 @@
+export function createCollisionObjects(scene, map) {
+    const collisionObjects = [];
+
+    const objectLayer = map.getObjectLayer('Object Layer 1');
+
+    objectLayer.objects.forEach(object => {
+        const centerX = object.x + object.width / 2;
+        const centerY = object.y + object.height / 2;
+
+        const collisionObject = scene.matter.add.rectangle(centerX, centerY, object.width, object.height, { isStatic: true });
+        collisionObjects.push(collisionObject);
+    });
+
+    return collisionObjects;
+}
+
+export function handleBarrierCollision(player, barrier) {
+    const overlapX = player.x - barrier.x;
+    const overlapY = player.y - barrier.y;
+
+    if (player.body.velocity.x > 0 && overlapX < 0) {
+        player.body.velocity.x = 0;
+        player.x = barrier.x - player.width / 2;
+    } else if (player.body.velocity.x < 0 && overlapX > 0) {
+        player.body.velocity.x = 0;
+        player.x = barrier.x + barrier.width + player.width / 2;
+    }
+
+    if (player.body.velocity.y > 0 && overlapY < 0) {
+        player.body.velocity.y = 0;
+        player.y = barrier.y - player.height / 2;
+    } else if (player.body.velocity.y < 0 && overlapY > 0) {
+        player.body.velocity.y = 0;
+        player.y = barrier.y + barrier.height + player.height / 2;
+    }
+}
