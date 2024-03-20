5d53ca5c9ff1f0427bdff343d3b214351a275065
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-19 23:11:38 -0400

    Update Player.js

--- natquest/js/scenes/Player.js
+++ natquest/js/scenes/Player.js
@@ -1,6 +1,6 @@
 export default class Player extends Phaser.Physics.Matter.Sprite {
     constructor(scene, x, y, texture) {
-        super(scene, x, y, texture);
+        super(scene.matter.world, x, y, texture);
         scene.add.existing(this);
         this.scene = scene;
     }
