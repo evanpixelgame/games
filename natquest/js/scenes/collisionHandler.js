6ab5a3a293fec8216f9fee4224e25adeb8be546b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-24 19:31:23 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -60,6 +60,65 @@ function calculateCentroid(vertices) {
 }
 
 
+export function createCollisionObjectsLayer2(scene, map) {
+    const collisionObjects = [];
+
+    const objectLayer2 = map.getObjectLayer('Object Layer 2');
+
+    objectLayer2.objects.forEach(object => {
+        // Create Matter.js bodies for collision objects in Object Layer 2
+        // Implement the logic similar to createCollisionObjects function for Object Layer 1
+                const centerX = object.x + object.width / 2;
+        const centerY = object.y + object.height / 2;
+
+        if (object.polygon) {
+            // Handle polygons
+            const polygonVertices = object.polygon.map(vertex => {
+                return { x: object.x + vertex.x, y: object.y + vertex.y };
+            });
+
+            // Adjust the centroid of the polygon
+            const centroid = calculateCentroid(polygonVertices);
+            const adjustedVertices = polygonVertices.map(vertex => {
+                return {
+                    x: vertex.x - centroid.x + centerX,
+                    y: vertex.y - centroid.y + centerY
+                };
+            });
+
+            const collisionObject = scene.matter.add.fromVertices(centerX, centerY, adjustedVertices, { isStatic: true });
+            collisionObjects.push(collisionObject);
+        } else if (object.ellipse) {
+            // Handle circles
+            const radiusX = object.width / 2;
+            const radiusY = object.height / 2;
+            const collisionObject = scene.matter.add.circle(centerX, centerY, Math.max(radiusX, radiusY), { isStatic: true });
+            collisionObjects.push(collisionObject);
+        } else {
+            // Handle rectangles
+            const collisionObject = scene.matter.add.rectangle(centerX, centerY, object.width, object.height, { isStatic: true });
+            collisionObjects.push(collisionObject);
+        }
+    });
+
+    return collisionObjects;
+}
+
+export function ObjectLayer2Handler(scene, player, objectLayer2CollisionObjects) {
+    // Check for collisions between the player and objects in Object Layer 2
+    objectLayer2CollisionObjects.forEach(object => {
+        scene.matterCollision.addOnCollideStart({
+            objectA: player,
+            objectB: object,
+            callback: () => {
+                // Transition to a new scene upon collision
+                scene.scene.start('InsideRoom');
+            }
+        });
+    });
+}
+
+
 export function handleBarrierCollision(player, barrier) {
     const overlapX = player.x - barrier.x;
     const overlapY = player.y - barrier.y;
