c83b5e06d145358bf985e37a55e2ab180b9591fe
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-24 01:11:52 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -21,61 +21,46 @@ export default class OpenWorld extends Phaser.Scene {
     
   }
 
-  create() {
-        // Create Matter.js engine
+create() {
+    // Create Matter.js engine
     this.matterEngine = this.matter.world;
- //  this.matterEngine.gravity.y = 0.5;
 
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
-      }
-     this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
-     this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
-   this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
+    }
+    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+    this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
+    this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
 
-  //Load map
-  const map = this.make.tilemap({ key: 'map' });
-  // Load tileset
-  const tileset = map.addTilesetImage('tilemap1', 'tiles');
+    // Load map
+    this.map = this.make.tilemap({ key: 'map' });
+    const tileset = this.map.addTilesetImage('tilemap1', 'tiles');
 
-  // Create layers ('Object Layer 1' layer creation is in collisionHanlder.js aka collision barrier layer 
-  const worldLayer = map.createLayer('Tile Layer 1', tileset, 0, 0);
-//  const worldObjectLayer = map.createLayer('Object Layer 1', tileset, 0, 0);
- // const worldCollisionObjectLayer = map.createLayer('Tile Layer 3', tileset, 0, 0);
+    // Create layers
+    const worldLayer = this.map.createLayer('Tile Layer 1', tileset, 0, 0);
 
+    // Convert tilemap to Matter.js physics bodies
+    this.matterEngine.convertTilemap(this.map);
 
-    this.player = new PlayerSprite(this, 15, 15, 'player'); // Create the player object
+    this.player = new PlayerSprite(this, 15, 15, 'player');
 
-    // Access player properties or methods as needed after it's created
-    const playerX = this.player.x;
-    const playerY = this.player.y;
-    
-  // Set world bounds for the player
-    const boundaryOffset = 2; // Adjust this value as needed
+    const boundaryOffset = 2;
     const worldBounds = new Phaser.Geom.Rectangle(
         boundaryOffset,
         boundaryOffset,
-        map.widthInPixels - 2 * boundaryOffset,
-        map.heightInPixels - 2 * boundaryOffset
+        this.map.widthInPixels - 2 * boundaryOffset,
+        this.map.heightInPixels - 2 * boundaryOffset
     );
 
-   this.matterEngine.setBounds(0, 0, worldBounds.width, worldBounds.height);
+    this.matterEngine.setBounds(0, 0, worldBounds.width, worldBounds.height);
 
-    
-    // Create collision objects
-    this.collisionObjects = createCollisionObjects(this, map);
-
-
-
-    //*****************************************CAMERA CONTROLS****************************************************
-  // Constrain the camera
-  this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
 
-        const startMenuScene = this.scene.get('StartMenu');
-        this.cameras.main.setZoom(2);
-    
-  } // <==== create func end tag    
+    const startMenuScene = this.scene.get('StartMenu');
+    this.cameras.main.setZoom(2);
+}
+
 
 //*****************************************************END OF CREATE FUNC ABOVE*******************************************************
 
