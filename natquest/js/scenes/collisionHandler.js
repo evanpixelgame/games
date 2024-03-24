c61f06a7d597a262dc28ff19199df70799edf89f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-23 23:08:53 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -7,33 +7,16 @@ export function createCollisionObjects(scene, map) {
         const centerX = object.x + object.width / 2;
         const centerY = object.y + object.height / 2;
 
-        if (object.polygon) {
-            // Handle polygons
-            const polygonVertices = object.polygon.map(vertex => {
-                return { x: vertex.x + object.x, y: vertex.y + object.y };
-            });
-            const collisionObject = scene.matter.add.polygon(centerX, centerY, polygonVertices, { isStatic: true });
-            collisionObjects.push(collisionObject);
-        } else {
-            // Handle rectangles
-            const collisionObject = scene.matter.add.rectangle(centerX, centerY, object.width, object.height, { isStatic: true });
-            collisionObjects.push(collisionObject);
-        }
+        const collisionObject = scene.matter.add.rectangle(centerX, centerY, object.width, object.height, { isStatic: true });
+        collisionObjects.push(collisionObject);
     });
 
     return collisionObjects;
 }
 
 export function handleBarrierCollision(player, barrier) {
-    let overlapX, overlapY;
-    if (barrier.type === 'rectangle') {
-        overlapX = player.body.position.x - barrier.bounds.min.x;
-        overlapY = player.body.position.y - barrier.bounds.min.y;
-    } else if (barrier.type === 'polygon') {
-        // For polygons, barrier.bounds is not available
-        overlapX = player.body.position.x - barrier.position.x;
-        overlapY = player.body.position.y - barrier.position.y;
-    }
+    const overlapX = player.x - barrier.x;
+    const overlapY = player.y - barrier.y;
 
     if (player.body.velocity.x > 0 && overlapX < 0) {
         player.body.velocity.x = 0;
