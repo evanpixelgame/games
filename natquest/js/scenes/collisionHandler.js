80f61d50a22aae9c7782c137714987af657fbe23
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-23 23:20:33 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -10,28 +10,13 @@ export function createCollisionObjects(scene, map) {
         const collisionObject = scene.matter.add.rectangle(centerX, centerY, object.width, object.height, { isStatic: true });
         collisionObjects.push(collisionObject);
     });
-    
-    if (object.polygon) {
-      const polygonVertices = object.polygon.map(vertex => {
-        return { x: vertex.x + object.x, y: vertex.y + object.y };
-  });
-        
-  const collisionObject = scene.matter.add.polygon(centerX, centerY, polygonVertices, { isStatic: true });
-  collisionObjects.push(collisionObject);
-}
 
     return collisionObjects;
 }
 
 export function handleBarrierCollision(player, barrier) {
-  let overlapX, overlapY;
-
-  // Use Matter.Query for collision detection with any shape
-  const collisionPairs = Matter.Query.overlaps(player.body, barrier);
-  if (collisionPairs.length > 0) {
-    const collisionPair = collisionPairs[0];
-    overlapX = collisionPair.overlap.x;
-    overlapY = collisionPair.overlap.y;
+    const overlapX = player.x - barrier.x;
+    const overlapY = player.y - barrier.y;
 
     if (player.body.velocity.x > 0 && overlapX < 0) {
         player.body.velocity.x = 0;
