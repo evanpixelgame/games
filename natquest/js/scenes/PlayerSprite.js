a7c010316cde95e0a8bf75aedb089589a6b6f067
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-25 16:52:48 -0400

    Update PlayerSprite.js

--- natquest/js/scenes/PlayerSprite.js
+++ natquest/js/scenes/PlayerSprite.js
@@ -1,6 +1,6 @@
 export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
     constructor(scene, x, y, texture) {
-        super(scene.matter.world, x, y, player);
+        super(scene.matter.world, x, y, texture);
 
         // Initialize the player sprite
         this.init();
