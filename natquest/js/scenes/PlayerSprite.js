879c2684a8af8c6bdb01590377c222c8f49d2da3
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-25 03:10:50 -0400

    Update PlayerSprite.js

--- natquest/js/scenes/PlayerSprite.js
+++ natquest/js/scenes/PlayerSprite.js
@@ -1,6 +1,6 @@
 export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
     constructor(scene, x, y, texture) {
-        super(scene.matter.world, x, y, texture);
+        super(scene.matter.world, x, y, 'player');
         scene.add.existing(this);
         this.scene = scene;
 
@@ -9,12 +9,14 @@ export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
     }
 
     init() {
-        // Set up the body type based on the actual shape of the sprite
+        const playerWidth = this.width;
+        const playerHeight = this.height;
+
         this.setBody({
-            type: 'rectangle', // Adjust this based on the shape of your sprite
-            width: this.width,
-            height: this.height,
-            isStatic: false, // Adjust this based on whether the sprite should be movable by physics
+            type: 'rectangle',
+            width: playerWidth / 2,
+            height: playerHeight / 2,
+            isStatic: false,
             restitution: 0,
             friction: 0.1,
             frictionAir: 0.02,
@@ -22,9 +24,12 @@ export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
 
         this.setScale(0.5);
 
-        // Resize the physics body if needed
         const scaledWidth = this.width * 0.5;
         const scaledHeight = this.height * 0.5;
+
         this.setSize(scaledWidth, scaledHeight);
     }
 }
+
+// Avoid exporting variables to the global scope if possible
+// window.player = Player;
