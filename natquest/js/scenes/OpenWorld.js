17ceac782c4594df887d45c370e808670ee28680
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-24 21:07:07 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -92,15 +92,6 @@ export default class OpenWorld extends Phaser.Scene {
         // Update logic if needed
     }
 
-    handleCollisionWithObjectLayer1() {
-        // Handle collision with Object Layer 1
-        // You can call the handleBarrierCollision function or perform any other necessary actions here
-                  if ((pair.bodyA === this.player.body || pair.bodyB === this.player.body) &&
-                (this.collisionObjects.includes(pair.bodyA) || this.collisionObjects.includes(pair.bodyB))) {
-                handleBarrierCollision(this.player, pair.bodyA === this.player.body ? pair.bodyB : pair.bodyA);
-            }
-    }
-
     handleCollisionWithObjectLayer2() {
         // Handle collision with Object Layer 2
         // Transition to the InsideRoom scene or perform any other necessary actions here
