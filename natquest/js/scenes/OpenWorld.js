f3b251e69bc287829515516f62618704da7ea1a6
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-24 21:13:27 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -1,5 +1,5 @@
 import { PlayerSprite } from './PlayerSprite.js';
-import { createCollisionObjects, createCollisionObjectsLayer2, ObjectLayer2handler, handleBarrierCollision,  } from './collisionHandler.js';
+import { createCollisionObjects, createCollisionObjectsLayer2, ObjectLayer2Handler, handleBarrierCollision,  } from './collisionHandler.js';
 
 export default class OpenWorld extends Phaser.Scene {
   constructor() {
@@ -111,30 +111,26 @@ for (let i = 0; i < map.layers.length; i++) {
   
   
 update(time, delta) {
-    // Handle collisions with Object Layer 1
+    // Handle collisions with Object Layer 1 and Object Layer 2
     Matter.Events.on(this.matter.world, 'collisionStart', (event) => {
         event.pairs.forEach((pair) => {
-            if (pair.bodyA === this.player.body || pair.bodyB === this.player.body) {
-                if (this.collisionObjects.includes(pair.bodyA) || this.collisionObjects.includes(pair.bodyB)) {
-                    handleBarrierCollision(this.player, pair.bodyA === this.player.body ? pair.bodyB : pair.bodyA);
-                }
+            // Collision with Object Layer 1
+            if ((pair.bodyA === this.player.body || pair.bodyB === this.player.body) &&
+                (this.collisionObjects.includes(pair.bodyA) || this.collisionObjects.includes(pair.bodyB))) {
+                handleBarrierCollision(this.player, pair.bodyA === this.player.body ? pair.bodyB : pair.bodyA);
             }
-        });
-    });
-
-    // Handle collisions with Object Layer 2
-    Matter.Events.on(this.matter.world, 'collisionStart', (event) => {
-        event.pairs.forEach((pair) => {
-            if (pair.bodyA === this.player.body || pair.bodyB === this.player.body) {
-                // Assuming Object Layer 2 collision objects are stored in this.collisionObjects2
-                if (this.collisionObjects2.includes(pair.bodyA) || this.collisionObjects2.includes(pair.bodyB)) {
-                    // Call the handler function to transition to the InsideRoom scene
-                    ObjectLayer2Handler(this);
-                }
+            // Collision with Object Layer 2
+            else if ((pair.bodyA === this.player.body || pair.bodyB === this.player.body) &&
+                (this.collisionObjects2.includes(pair.bodyA) || this.collisionObjects2.includes(pair.bodyB))) {
+                // Call the handler function to transition to the InsideRoom scene
+                console.log('should be transitioning scenes msg coming from open world scene');
+                ObjectLayer2Handler(this, this.player, pair.bodyA === this.player.body ? pair.bodyB : pair.bodyA);
             }
         });
     });
 }
+
+
 
   
 }
