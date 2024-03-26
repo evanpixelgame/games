d510e20ee963e3b08006d2064165132b0c1ada98
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 04:02:37 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -21,6 +21,7 @@ export default class OpenWorld extends Phaser.Scene {
 
   init(data) {
         this.openWorldScene = data.OpenWorld;
+        this.controls = this.scene.get('ComputerControls');
   }
       
   preload() {
@@ -41,11 +42,11 @@ console.log('HELLO THERE PLEASE LOG ' + this.world);
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
     }
-
-   this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }, (scene) => {
+   this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+ //this.controls = this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+  /* this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }, (scene) => {
   this.controls = scene; // Scene instance passed as argument
-});
-  //  this.controls = this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+}); */
     this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
     this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
 
