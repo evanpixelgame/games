00f4b5c3dff38d13fcf07e8727c1a615fa7d4e6b
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 17:14:34 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -100,7 +100,7 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     this.player.setVelocityY(0); // Set initial Y velocity to 0
     
  //   this.controls = null;
-  // this.controls = this.scene.get('ComputerControls');
+   this.controls = this.scene.get('ComputerControls');
     
    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
 
