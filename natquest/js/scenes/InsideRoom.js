5ac671b1fb4dc1d63ed0b5ae3da279806cf2cf0d
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 18:13:29 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -103,16 +103,7 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
       this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
     this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }); // Launch ComputerControls scene
-    //  this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
-    
-  //  this.player.setVelocityX(0); // Set initial X velocity to 0
-  //  this.player.setVelocityY(0); // Set initial Y velocity to 0
-    
- //   this.controls = null;
- //  this.controls = this.scene.get('ComputerControls');
-    
- //  this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
-
+this.speed = 2;
     
     // Set world bounds for the player
     const boundaryOffset = 2;
