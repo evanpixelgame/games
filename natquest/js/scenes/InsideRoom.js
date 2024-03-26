a895e2f81e37da0c4f05cf38567fcdd448d35aa9
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 00:50:26 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -22,12 +22,9 @@ export default class InsideRoom extends Phaser.Scene {
 
          // Load tileset
     const tilesetsData = [
-      //  { name: 'tilesheetTerrain', key: 'tilesheetTerrain' },
         { name: 'tilesheetInterior', key: 'tilesheetInterior' },
-     //   { name: 'tilesheetBuildings', key: 'tilesheetBuildings' },
         { name: 'tilesheetWalls', key: 'tilesheetWalls' },
         { name: 'tilesheetObjects', key: 'tilesheetObjects' },
-      //  { name: 'tilesheetFlourishes', key: 'tilesheetFlourishes' }
     ];
 
     const tilesets = [];
@@ -40,16 +37,11 @@ export default class InsideRoom extends Phaser.Scene {
     for (let i = 0; i < map.layers.length; i++) {
         layers.push(map.createLayer(i, tilesets, 0, 0));
     }
-   this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-      
-        // Load tilesets and create layers similar to how it's done in the OpenWorld scene
-        // Adjust camera bounds and follow the player if needed
-        // Add any additional setup specific to this scene
     }
 
-    // Optionally, you may need to define an update method if this scene requires regular updates
+
     update(time, delta) {
-        // Update method code here
+     
     }
 }
 window.InsideRoom = InsideRoom;
