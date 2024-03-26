61030ac18be9d9344439c40bdb25ab2cbf2285a3
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 05:31:19 -0400

    Update ComputerControls.js

--- natquest/js/scenes/ComputerControls.js
+++ natquest/js/scenes/ComputerControls.js
@@ -65,7 +65,8 @@ update(time, delta) {
     }
 
     // Set the velocity of the player sprite
-    this.player.setVelocity(velocityX, velocityY);
+   // this.player.setVelocity(velocityX, velocityY);
+    this.player.body.setVelocity(velocityX, velocityY);
   
 
     // Play appropriate animation based on movement direction
