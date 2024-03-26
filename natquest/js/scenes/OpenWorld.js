f2b841ab19468ab21800728508cc745a5c563a20
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 19:03:55 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -41,9 +41,7 @@ console.log('HELLO THERE PLEASE LOG ' + this.world);
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
     }
- //  this.controls = null;
- //  this.controls = this.scene.get('ComputerControls'); 
- 
+   
   
 
    
@@ -82,8 +80,6 @@ console.log('HELLO THERE PLEASE LOG ' + this.world);
   // Listen for the 'created' event on the player sprite
             const playerBodyWorld = this.player.body ? this.player.body.world : null;
         console.log('Player Body World123:', playerBodyWorld);
-
-     this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
           
       
     console.log(this.player.body);
@@ -94,7 +90,10 @@ console.log('Player GameObject:', this.player.gameObject);
              console.log('Player Body GameObject layer:', this.player.body.gameObject.layer);
 
   //  console.log('Player Layer Index:', this.player.body.gameObject.layer.index);
-
+this.controls = null;
+   this.controls = this.scene.get('ComputerControls');
+    
+   this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
 // Set world bounds for the player
 const boundaryOffset = 2; // Adjust this value as needed
 const worldBounds = new Phaser.Geom.Rectangle(
