5c5d225a36b89984e982c5a5ebe781a8d52093a5
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-27 19:20:26 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -146,7 +146,7 @@ this.TransitionSensorHandler(this.player, this.transitionSensors);
     
 }            
              console.log('COLLISIONTILE FROM OPENWORLD CUSTOM ID:' + otherBody)
-             console.log('COLLISIONTILE FROM OPENWORLD CUSTOM ID:' + otherBody.properties)
+             console.log('COLLISIONTILE FROM OPENWORLD CUSTOM ID:' + otherBody.properties.value)
              console.log('COLLISIONTILE FROM OPENWORLD CUSTOM ID:' + pair.bodyB)
              
              if (otherBody.id == 25) {
