f005123094d5d8d0dc3755c75cd3337dc0601533
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-27 04:44:06 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -88,11 +88,11 @@ create() {
 //  this.player = new PlayerSprite(this, 500, 500, 'player');
 
     // Initialize the player sprite
-    this.player.init();
+  // this.player.init();
 
     // Add the player sprite to the scene
-    this.add.existing(this.player);
-    this.matter.add.gameObject(this.player);
+//    this.add.existing(this.player);
+  //  this.matter.add.gameObject(this.player);
 
     // Set the world property to the scene's matter world
 //    this.player.world = this.matter.world;
