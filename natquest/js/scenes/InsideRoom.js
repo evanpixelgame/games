00069b6b3bb12408cbcf58d86572bb2db28a6b16
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-26 20:10:57 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -116,7 +116,7 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
    // this.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
 
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+ //   this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
 
        console.log("InsideRoom end of create func status with:", {
         player: this.player,
