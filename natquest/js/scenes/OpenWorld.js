7573865822ec97447ee1e9eab07db3ae849eb94a
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-27 18:55:31 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -147,7 +147,9 @@ this.TransitionSensorHandler(this.player, this.transitionSensors);
 }            
              
            //  if (otherBody.id == 25) {
-              if ((object.properties.find(prop => prop.name === 'customID')?.value) == 'transitionSensor') {
+               if (otherBody.gameObject && otherBody.gameObject.properties) {
+                    const customIDProperty = otherBody.gameObject.properties.find(prop => prop.name === 'customID');
+                    if (customIDProperty && customIDProperty.value === 'transitionSensor') {
    console.log('youve hit the sensor by the door');
                this.scene.remove('ComputerControls');
   this.scene.start('InsideRoom', {
