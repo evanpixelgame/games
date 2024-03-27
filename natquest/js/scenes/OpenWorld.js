7d44600cc241902eb2e485afa7072597688eb455
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-27 19:00:22 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -141,6 +141,7 @@ this.TransitionSensorHandler(this.player, this.transitionSensors);
                 const otherBody = pair.bodyA === player.body ? pair.bodyB : pair.bodyA;
                 // Log the ID of the other object
                 console.log('Collision detected with object ID:', otherBody.id);
+            }
               if (otherBody.id == 19) {
    console.log('youve hit the farming pen');
     
