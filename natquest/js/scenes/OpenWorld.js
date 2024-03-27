47d5c321d5e66d723d9392e3d5a9f2079c2c20e1
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-27 06:39:49 -0400

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -1,12 +1,14 @@
-import PlayerSprite from './PlayerSprite.js';
-import ComputerControls from './ComputerControls.js';
-import { createCollisionObjects, createTransitionSensors, handleBarrierCollision } from './collisionHandler.js';
+import { PlayerSprite } from './PlayerSprite.js';
+import { createCollisionObjects, 
+ createTransitionSensors, 
+      // TransitionSensorHandler,
+        handleBarrierCollision } from './collisionHandler.js';
 
 export default class OpenWorld extends Phaser.Scene {
   constructor() {
     super({ key: 'OpenWorld' });
     
-    // Declare controls as a property of the class AKA set up the properties as blank variables for stuff you intend to share with other scenes
+    // Declare controls as a property of the class (should I delete these and put in the init func?
    this.controls = null;
     this.map = null;
     this.player = null;
@@ -19,9 +21,6 @@ export default class OpenWorld extends Phaser.Scene {
 
   init(data) {
         this.openWorldScene = data.OpenWorld;
-     this.player = data.player;
-    this.speed = data.speed;
-    console.log("Received player in ComputerControls:", this.player); // 
   }
       
   preload() {
@@ -30,31 +29,32 @@ export default class OpenWorld extends Phaser.Scene {
        
   
   
-create() {
+  create() {
     // Create Matter.js engine
     this.matterEngine = Phaser.Physics.Matter.Matter.World;
     this.engine = this.matter.world;
-    this.world = this.matterEngine.create({
-        // your Matter.js world options here
-    });
-    console.log('Matter.js world:', this.world);
-
+//    this.world = this.engine.world;
+     this.world = this.matterEngine.create({
+    // your Matter.js world options here
+  });
+console.log('HELLO THERE PLEASE LOG ' + this.world);
     if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
         this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
     }
-
-   this.player = new PlayerSprite(this, 495, 325, 'player');
-     // this.player.setVelocity(velocityX, velocityY);
-  this.player.setVelocity(0, 0);
+ //  this.controls = null;
+ //  this.controls = this.scene.get('ComputerControls'); 
+ 
   
-  
-    console.log("Player object in OpenWorld:", this.player);
-    this.controls = new ComputerControls();
-  this.controls.setPlayer(this.player);
-    // Launch ComputerControls scene
-    this.scene.add('ComputerControls', ComputerControls);
-    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
 
+   
+ //this.controls = this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+  /* this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }, (scene) => {
+  this.controls = scene; // Scene instance passed as argument
+}); */
+    this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
+    this.scene.launch('CompUI', { OpenWorld: this, player: this.player, speed: this.speed, map: this.map, camera: this.cameras.main });
+
+    // Load map
     const map = this.make.tilemap({ key: 'map' });
 
     // Load tileset
@@ -78,70 +78,61 @@ create() {
         layers.push(map.createLayer(i, tilesets, 0, 0));
     }
 
-  
-    // Initialize controls after creating the player sprite
+    this.player = new PlayerSprite(this, 495, 325, 'player'); // Create the player object, just took away this.world as 2nd argument
+  // Listen for the 'created' event on the player sprite
+            const playerBodyWorld = this.player.body ? this.player.body.world : null;
+        console.log('Player Body World123:', playerBodyWorld);
 
-    // Define world bounds based on map dimensions
-    const boundaryOffset = 2; // Adjust this value as needed
-    const worldBounds = new Phaser.Geom.Rectangle(
-        boundaryOffset,
-        boundaryOffset,
-        map.widthInPixels - 2 * boundaryOffset,
-        map.heightInPixels - 2 * boundaryOffset
-    );
+     this.scene.launch('ComputerControls', { player: this.player, speed: this.speed });
+          
+      
+    console.log(this.player.body);
+    console.log('Player World:', this.player.body.world);
+    console.log('Player Body:', this.player.body);
+console.log('Player GameObject:', this.player.gameObject);
+          console.log('Player Body GameObject:', this.player.body.gameObject);
+             console.log('Player Body GameObject layer:', this.player.body.gameObject.layer);
 
-//  this.player = new PlayerSprite(this, 500, 500, 'player');
+  //  console.log('Player Layer Index:', this.player.body.gameObject.layer.index);
 
-    // Initialize the player sprite
-  // this.player.init();
+// Set world bounds for the player
+const boundaryOffset = 2; // Adjust this value as needed
+const worldBounds = new Phaser.Geom.Rectangle(
+    boundaryOffset,
+    boundaryOffset,
+    map.widthInPixels - 2 * boundaryOffset,
+    map.heightInPixels - 2 * boundaryOffset
+);
 
-    // Add the player sprite to the scene
-//    this.add.existing(this.player);
-  //  this.matter.add.gameObject(this.player);
-
-    // Set the world property to the scene's matter world
-//    this.player.world = this.matter.world;
-
+this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
+          
+           console.log(this.world);
+    // Create collision objects
     this.collisionObjects = createCollisionObjects(this, map);
-    this.transitionSensors = createTransitionSensors(this, map, this.player);
+    this.transitionSensors = createTransitionSensors(this, map, this.player); 
 
-    // Use TransitionSensorHandler to handle collision events with transition sensors
-    //this.TransitionSensorHandler(this.player, this.transitionSensors);
-    this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
+  // Use TransitionSensorHandler to handle collision events with transition sensors
+this.TransitionSensorHandler(this.player, this.transitionSensors);
+          
+
+
+    // Constrain the camera
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-  //  this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+
+    const startMenuScene = this.scene.get('StartMenu');
     this.cameras.main.setZoom(2);
 
-  this.speed = 2;
-       this.scene.start('InsideRoom', {
-  player: this.player,
-  speed: this.speed,
-  camera: this.cameras.main,
-  controls: this.controls, // Passing the controls object here
-  engine: this.matter.world,
- // world: scene.engine.world,
-   world: this.world,
-});
+   console.log('PLEASE PLEASE LOG' + this.world);
+   console.log('PRETTY PLEASE' + this.controls);
+   console.log('PRETY PLZ LOG VELOCITY FROM OPENWORLD: ' + this.velocity);
 
-}
-/*
-    console.log('Player:', this.player);
-    console.log('Player body:', this.player.body);
-    console.log('Player body world:', this.player.body ? this.player.body.world : null);
-    console.log('Player GameObject:', this.player.gameObject);
-    console.log('Player Body GameObject:', this.player.body.gameObject);
-    console.log('Player Body GameObject layer:', this.player.body.gameObject.layer);
-    console.log('Controls:', this.controls);
-    console.log('Camera:', this.cameras.main);
-    console.log('OpenWorld scene:', this);
-    */
+  }
 
 
-
-/*
- TransitionSensorHandler(scene, player, transitionSensors) {
+ TransitionSensorHandler(player, transitionSensors) {
     // Listen for collisionstart event on the world property of the scene where the player is created
-    this.player.scene.matter.world.on('collisionstart', (scene, eventData) => {
+    this.player.scene.matter.world.on('collisionstart', (eventData) => {
         // Loop through pairs of colliding bodies
         eventData.pairs.forEach(pair => {
             // Check if the player is one of the bodies involved in the collision
@@ -157,34 +148,22 @@ create() {
              
              if (otherBody.id == 25) {
    console.log('youve hit the sensor by the door');
-             //  this.scene.remove('ComputerControls');
-  scene.scene.start('InsideRoom', {
-  player: scene.player,
-  speed: scene.speed,
-  camera: scene.cameras.main,
-  controls: scene.controls, // Passing the controls object here
-  engine: scene.matter.world,
- // world: scene.engine.world,
-   world: scene.world,
+               this.scene.remove('ComputerControls');
+  this.scene.start('InsideRoom', {
+  player: this.player,
+  speed: this.speed,
+  camera: this.cameras.main,
+  controls: this.controls, // Passing the controls object here
+  engine: this.matter.world,
+ // world: this.engine.world,
+   world: this.world,
 });
 
 }
             }
         });
     });
-this.speed = 2;
-       this.scene.start('InsideRoom', {
-  player: this.player,
-  speed: this.speed,
-  camera: this.cameras.main,
-  controls: this.controls, // Passing the controls object here
-  engine: this.matter.world,
- // world: scene.engine.world,
-   world: this.world,
-});
 }
-
-  */
 
         
   update(time, delta) {
