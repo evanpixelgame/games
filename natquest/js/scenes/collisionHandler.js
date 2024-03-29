5d849eaea659ef42c990b05184c4ae2a284aabe9
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-29 13:19:53 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -1,18 +1,22 @@
 import { gameManager } from '../gameState.js';
 
 export function sensorMapSet(scene, map) {
- const objectLayer2 = map.getObjectLayer('Object Layer 2');
+    const objectLayer2 = map.getObjectLayer('Object Layer 2');
 
     objectLayer2.objects.forEach(object => {
         // Log object properties to check if it has the customID property
         const customIDProperty = object.properties.find(prop => prop.name === 'customID');
         const customID = customIDProperty ? customIDProperty.value : null;
         console.log('Object Custom IDfromhandler:', customID);
-        gameManager.sensorID[customIDProperty] = customID;
+
+        // Assign value to gameManager.sensorID using customID as the key
+        gameManager.sensorID[customID] = object; // Assuming you want to store the entire object here
+
         const centerX = object.x + object.width / 2;
         const centerY = object.y + object.height / 2;
         const width = object.width;
         const height = object.height;
+
         // Create the rectangle sensor body
         const sensor = scene.matter.add.rectangle(centerX, centerY, width, height, {
             isSensor: true, // Set to true to make it a sensor
@@ -22,8 +26,10 @@ export function sensorMapSet(scene, map) {
             }
         });   
     });
- console.log(`NATALY IS THE MOST BEAUTIFUL POPULATE OBJECT` + gameManager.sensorID);
+
+    console.log(`NATALY IS THE MOST BEAUTIFUL POPULATE OBJECT`, gameManager.sensorID);
 }
+
 //I want it so the key is the customID (aka, transitionsensor to be renamed openworldtoInsideRoom) and the value to be the matter.js body that the sensor is associated with.
 
 
