d97f5b4e3d5ddb777a7e4dc42cfaff5a9e53a6e7
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-28 22:33:30 -0400

    Update collisionHandler.js

--- natquest/js/scenes/collisionHandler.js
+++ natquest/js/scenes/collisionHandler.js
@@ -1,4 +1,3 @@
-//const sensorID = new Map();
 let sensorID = {};
 
 export function sensorMapSet(scene, map) {
@@ -36,7 +35,7 @@ export function sensorMapSet(scene, map) {
 
 
  
-export function SensorHandler(scene, map, player, transitionSensors, sensorID) {
+export function sensorHandler(scene, map, player, transitionSensors, sensorID) {
     const objectLayer2 = map.getObjectLayer('Object Layer 2');
 
     objectLayer2.objects.forEach(object => {
@@ -64,72 +63,6 @@ export function SensorHandler(scene, map, player, transitionSensors, sensorID) {
                  const key = 'transitionSensor'; // initalize
                   const sensorObject = sensorID[key];
                     switch (sensorObject) {
-                        case 'transitionSensor':
-                            console.log('You hit a transition sensor!');
-                            // Perform actions specific to this customID
-                            console.log('youve hit the sensor by the door');
-                            scene.scene.remove('ComputerControls');
-                            scene.scene.start('InsideRoom', {
-                                player: scene.player,
-                                speed: scene.speed,
-                                camera: scene.cameras.main,
-                                controls: scene.controls, // Passing the controls object here
-                                engine: scene.matter.world,
-                                world: scene.world,
-                            });
-                            break;
-                        // Add more cases for other customIDs as needed
-                        default:
-                            // Handle other customIDs
-                            break;
-                    }
-                }
-            }
-        });
-    });
-         
-    }); 
-}
-
-
-
-
-
-
-
-
-
-
- 
- 
-
-export function SensorHandler(scene, map, player, transitionSensors, sensorID) {
-    const objectLayer2 = map.getObjectLayer('Object Layer 2');
-
-    objectLayer2.objects.forEach(object => {
-        // Log object properties to check if it has the customID property
-        console.log('Object ID:', object.id);
-        const customIDProperty = object.properties.find(prop => prop.name === 'customID');
-        const customID = customIDProperty ? customIDProperty.value : null;
-        console.log('Object Custom IDfromhandler:', customID);
-                                 
-                                 
-    // Listen for collisionstart event on the world property of the scene where the player is created
-    player.scene.matter.world.on('collisionstart', (eventData) => {
-        // Loop through pairs of colliding bodies
-        eventData.pairs.forEach(pair => {
-            // Check if the player is one of the bodies involved in the collision
-            if (pair.bodyA === player.body || pair.bodyB === player.body) {
-                // Get the other body involved in the collision
-                const otherBody = pair.bodyA === player.body ? pair.bodyB : pair.bodyA;
-                // Log the ID of the other object
-                console.log('Collision detected with object ID:', otherBody.id);
-                 console.log(customID);
-                
-                // Check if the other body has a customID property
-               if (customID) {
-
-                    switch (customID) {
                         case 'transitionSensor':
                             console.log('You hit a transition sensor!');
                             // Perform actions specific to this customID
@@ -219,65 +152,6 @@ function calculateCentroid(vertices) {
 
     return { x: centroidX, y: centroidY };
 }
-
-
-
-
-export function createTransitionSensors(scene, map, sensorID) {
-    const transitionSensors = {};
-
-    const objectLayer2 = map.getObjectLayer('Object Layer 2');
-
-    //const sensorIDs = sensorID;
-
-    objectLayer2.objects.forEach(object => {
-        // Log object properties to check if it has the customID property
-        console.log('Object ID:', object.id);
-        const customIDProperty = object.properties.find(prop => prop.name === 'customID');
-        const customID = customIDProperty ? customIDProperty.value : null;
-      
-        
-        const key = object.name; // Assuming a 'name' property exists
-          // Add the object to the Map with the extracted key
-        sensorID.set(key, object);
-        
-       
-        
-        console.log('Object Custom ID:', customID);
-
-        const centerX = object.x + object.width / 2;
-        const centerY = object.y + object.height / 2;
-
-        // Calculate sensor dimensions
-        const width = object.width;
-        const height = object.height;
-
-        // Create the rectangle sensor body
-        const sensor = scene.matter.add.rectangle(centerX, centerY, width, height, {
-            isSensor: true, // Set to true to make it a sensor
-            render: {
-                fillStyle: 'transparent', // Optional: make the sensor invisible
-                strokeStyle: 'red' // Optional: set a stroke color for debugging
-            }
-        });
-
-        // Add the sensor to the transitionSensors object with the customID as the key
-        if (customID) {
-            transitionSensors[customID] = sensor;
-        }
-
-        // Log sensor properties
-        console.log('Sensor:', sensor);
-    });
-
-    // Log all transition sensors
-    console.log('Transition Sensors:', transitionSensors);
-    Object.entries(transitionSensors).forEach(([key, value]) => {
-    console.log(`Key: ${key}, Value:`, value);
-});
-    return transitionSensors;
-}
-
 
 
 export function handleBarrierCollision(player, barrier) {
