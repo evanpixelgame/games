a088f74249d332545805e84f014de9772632262e
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-30 19:15:32 -0400

    Update ComputerControls.js

--- natquest/js/scenes/ComputerControls.js
+++ natquest/js/scenes/ComputerControls.js
@@ -24,12 +24,12 @@ export class ComputerControls extends Phaser.Physics.Matter.Sprite {
 
   create() {
     // Create controls for arrow keys and WASD
-    this.cursors = this.scene.input.keyboard.addKeys({
+/*    this.cursors = this.scene.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
       down: Phaser.Input.Keyboard.KeyCodes.S,
       left: Phaser.Input.Keyboard.KeyCodes.A,
       right: Phaser.Input.Keyboard.KeyCodes.D,
-    });
+    }); */
   }
 
 update(time, delta) {
