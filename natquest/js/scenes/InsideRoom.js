b44e98b0025a200dcee86cd1ef2d8900fef0cedf
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-30 02:54:44 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -96,7 +96,7 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     }
     this.speed = 2;
     // Initialize player sprite
-    this.player = new PlayerSprite(this, 495, 325, 'player');
+    this.player = new PlayerSprite(this, 970, 664, 'player');
     
     this.player.setScale(1); 
 
