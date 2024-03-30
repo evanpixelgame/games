f25960269e3f1c03a551d6d0c9191efe2241f2e2
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-30 10:36:53 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -1,5 +1,4 @@
 import { PlayerSprite } from './PlayerSprite.js';
-//import ComputerControls from './ComputerControls.js';
 import { sensorMapSet, createCollisionObjects, sensorHandler } from './collisionHandler.js';
 
 export default class InsideRoom extends Phaser.Scene {
@@ -9,8 +8,8 @@ export default class InsideRoom extends Phaser.Scene {
 
 
   init(data) {
-  //    this.controls = data.controls || null;
-    //  this.engine = data.engine || null;
+      this.controls = data.controls || null;
+      this.engine = data.engine || null;
     // Check if the necessary data is provided
 if (!data || !data.player || !data.speed || !data.camera || !data.controls || !data.engine || !data.world) {
     let missingData = [];
@@ -93,16 +92,6 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     for (let i = 0; i < map.layers.length; i++) {
         layers.push(map.createLayer(i, tilesets, 0, 0));
     }
-   // this.speed = 2;
-    // Initialize player sprite
-   // this.player = new PlayerSprite(this, 970, 664, 'player');
-    
- //   this.player.setScale(1); 
-
-//    this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
-  //    this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
-   // this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }); // Launch ComputerControls scene
-
     
     // Set world bounds for the player
     const boundaryOffset = 2;
@@ -112,13 +101,11 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
         map.widthInPixels - 2 * boundaryOffset,
         map.heightInPixels - 2 * boundaryOffset
     );
-   // this.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
 
       this.collisionObjects = createCollisionObjects(this, map);
       this.sensorMapping = sensorMapSet(this, map, this.sensorID);  
       this.sensorHandling = sensorHandler(this, map, this.player);
 
-    
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
 
