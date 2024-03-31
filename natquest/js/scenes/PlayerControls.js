ed06629e6770eae2064007af7a64286a17d690ce
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 19:54:48 -0400

    Create PlayerControls.js

---
+++ natquest/js/scenes/PlayerControls.js
@@ -0,0 +1,80 @@
+export class PlayerControls extends Phaser.Physics.Matter.Sprite {
+    constructor(scene, player, x, y) {
+        super(scene.matter.world, x, y, texture);
+
+        // Initialize the player sprite
+        this.init();
+        // Add the player sprite to the scene
+        scene.add.existing(this);
+        this.body = this.body;
+        this.gameObject = this;
+
+    }
+    
+    init() {
+ 
+    }
+
+create() {
+
+  
+}
+
+  
+  update(time, delta) {
+
+      if (!this.player) {
+        return;
+    }
+    let velocityX = 0;
+    let velocityY = 0;
+
+    // Determine velocity based on key presses
+    if (this.cursors.up.isDown) {
+        velocityY = -this.speed;
+    } else if (this.cursors.down.isDown) {
+        velocityY = this.speed;
+    }
+
+    if (this.cursors.left.isDown) {
+        velocityX = -this.speed;
+    } else if (this.cursors.right.isDown) {
+        velocityX = this.speed;
+    }
+
+    // Normalize velocity to prevent faster movement diagonally
+    if (velocityX !== 0 && velocityY !== 0) {
+        const magnitude = Math.sqrt(velocityX * velocityX + velocityY * velocityY);
+        velocityX *= this.speed / magnitude;
+        velocityY *= this.speed / magnitude;
+    }
+
+    // Set the velocity of the player sprite
+    this.player.setVelocity(velocityX, velocityY);
+ this.player.body.velocity.x = velocityX;
+this.player.body.velocity.y = velocityY;
+
+ 
+//  console.log(this.player);
+//    console.log(this.player.body);
+//    console.log(this.player.body.velocity);
+
+    // Play appropriate animation based on movement direction
+    if (velocityX !== 0 || velocityY !== 0) {
+        if (velocityX > 0) {
+            this.player.anims.play('walking-right', true);
+        } else if (velocityX < 0) {
+            this.player.anims.play('walking-left', true);
+        } else if (velocityY < 0) {
+            this.player.anims.play('walking-down', true);
+        } else if (velocityY > 0) {
+            this.player.anims.play('walking-up', true);
+        }
+    } else {
+        // Stop animation when no movement
+        this.player.anims.stop();
+    }
+   this.player.setRotation(0);
+}
+  
+}
