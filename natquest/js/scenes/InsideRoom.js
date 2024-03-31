7c3b02efde08005c18909a14d473c89eb8282865
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 15:20:14 -0400

    Update InsideRoom.js

--- natquest/js/scenes/InsideRoom.js
+++ natquest/js/scenes/InsideRoom.js
@@ -1,7 +1,6 @@
 import { PlayerSprite } from './PlayerSprite.js';
+import ComputerControls from './ComputerControls.js';
 import { sensorMapSet, createCollisionObjects, sensorHandler } from './collisionHandler.js';
-//import { ComputerControls } from './ComputerControls.js';
-
 
 export default class InsideRoom extends Phaser.Scene {
   constructor() {
@@ -101,9 +100,9 @@ if (!data || !data.player || !data.speed || !data.camera || !data.controls || !d
     
     this.player.setScale(1); 
 
-  //  this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
-    //  this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
-   // this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }); // Launch ComputerControls scene
+    this.scene.add('ComputerControls', ComputerControls); // Add ComputerControls scene
+      this.controls = this.scene.get('ComputerControls'); // Retrieve controls scene
+    this.scene.launch('ComputerControls', { player: this.player, speed: this.speed }); // Launch ComputerControls scene
 
     
     // Set world bounds for the player
