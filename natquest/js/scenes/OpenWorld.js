2001ccfed38e0e120924103514ee7b6210e4521f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 00:54:37 -0500

    Update OpenWorld.js

--- natquest/js/scenes/OpenWorld.js
+++ natquest/js/scenes/OpenWorld.js
@@ -78,7 +78,14 @@ this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
 
     const startMenuScene = this.scene.get('StartMenu');
     this.cameras.main.setZoom(2);
-    this.cursors = this.input.keyboard.createCursorKeys();
+   // this.cursors = this.input.keyboard.createCursorKeys();
+      this.cursors = this.input.keyboard.addKeys({
+    up: Phaser.Input.Keyboard.KeyCodes.W,
+    down: Phaser.Input.Keyboard.KeyCodes.S,
+    left: Phaser.Input.Keyboard.KeyCodes.A,
+    right: Phaser.Input.Keyboard.KeyCodes.D,
+  });
+
 
   }
 
