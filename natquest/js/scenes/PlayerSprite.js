4bde103064237d84543ece2e631135fcde7cad92
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 15:22:55 -0400

    Update PlayerSprite.js

--- natquest/js/scenes/PlayerSprite.js
+++ natquest/js/scenes/PlayerSprite.js
@@ -1,5 +1,5 @@
 export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
-    constructor(scene, x, y, texture, initialVelocityX, initialVelocityY) {
+    constructor(scene, x, y, texture) {
         super(scene.matter.world, x, y, texture);
 
         // Initialize the player sprite
@@ -7,9 +7,15 @@ export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
 
         // Add the player sprite to the scene
         scene.add.existing(this);
-
-        // Set the initial velocity
-        this.setVelocity(initialVelocityX, initialVelocityY);
+        
+        // Set the world property to the scene's matter world
+      //  this.world = scene.matter.world;
+        this.body = this.body;
+       // this.player.body.setVelocity(velocityX, velocityY);
+        this.gameObject = this;
+       // this.layer = layer;
+         //this.setVelocity = this.setVelocity.bind(this); // Bind setVelocity to this instance
+     //   this.player.setVelocity(this.player.body, velocityX, velocityY);
     }
     
     init() {
@@ -31,5 +37,11 @@ export class PlayerSprite extends Phaser.Physics.Matter.Sprite {
         const scaledWidth = playerWidth * 0.5;
         const scaledHeight = playerHeight * 0.5;
         this.setSize(scaledWidth, scaledHeight);
+           // this.body.setVelocity(velocityX, velocityY);
+            console.log(this.body);
+            console.log(this.world);
+            console.log('Player Body Worldfromplaysprite:', this.body.world);
+       
+ 
     }
 }
