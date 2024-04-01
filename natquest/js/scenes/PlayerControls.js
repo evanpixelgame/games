8b443e7fe48a8f6b1d60fdc9b7e0c628dd24dab7
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 22:06:26 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -5,13 +5,13 @@ export class PlayerControls {
         this.player = player;
         this.velocity = velocity;
         this.world = world;
-
+/*
         this.cursors = this.input.keyboard.addKeys({
     up: Phaser.Input.Keyboard.KeyCodes.W,
     down: Phaser.Input.Keyboard.KeyCodes.S,
     left: Phaser.Input.Keyboard.KeyCodes.A,
     right: Phaser.Input.Keyboard.KeyCodes.D,
-  });
+  }); */
     }
     
     init() {
