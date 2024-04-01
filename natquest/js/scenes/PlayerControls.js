3f03812aaaa198b4d7cd99a905ecabed7b350743
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-03-31 22:52:27 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -1,9 +1,9 @@
 export default class PlayerControls {
   constructor(scene, player, velocity, world) {
-    this.scene = scene;
-    this.player = player;
-    this.velocity = velocity;
-    this.world = world;
+    this.scene = data.scene;
+    this.player = data.player;
+    this.velocity = data.velocity;
+    this.world = data.world;
 
     this.cursors = this.scene.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
